4b6c631b928ca875160c5e4deb1e08e5f556c442
AntonioMakdissi <antoniomakdissi1@gmail.com>
2024-10-01 13:24:58 +0200

    tried to fix projects web2

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -11,10 +11,10 @@ let render_projects = (slug) => {
 
     let projects_obj = [
         {
-            image: 'assets/images/mentors.jpg',
-            link: 'https://github.com/abhn/Mporter',
+            image: 'assets/images/speedrun.jpg',
+            link: 'https://github.com/AntonioMakdissi/seniorProject',
             title: 'SpeedRun',
-            demo: 'https://mporter.co',
+            demo: 'https://antoniomakdissi.github.io/seniorProject/',
             technologies: ['PHP', 'CSS', 'Javascript', 'SQL', 'HTML'],
             description: "Delivery web application with mobile app and tracking. Multi-user support, easy to deploy and use. (Senior Project)",
             categories: ['featured', 'webdev']
@@ -36,15 +36,6 @@ let render_projects = (slug) => {
             technologies: ['Android', 'OpenCV'],
             description: "Attendance marking tool that uses face recognition for marking attendance and firebase for tracking and analytics.",
             categories: ['featured', 'native']
-        },
-        {
-            image: 'assets/images/mpw.jpg',
-            link: 'https://github.com/abhn/mpw',
-            title: 'Master Password',
-            demo: 'https://www.nagekar.com/mpw',
-            technologies: ['Semantic UI', 'CSS3'],
-            description: "Master Password is an ingenious password solution that makes your passwords truly impossible to lose.",
-            categories: ['featured', 'security']
         },
         {
             image: 'assets/images/social-share-count.jpeg',
@@ -80,7 +71,7 @@ let render_projects = (slug) => {
             demo: false,
             technologies: ['python'],
             description: "Automate crawling of a website and find publicly open S3 buckets for takeover.",
-            categories: ['native', 'security']
+            categories: ['native', 'reasearch']
         },
         {
             image: 'assets/images/elementary.png',
@@ -92,24 +83,6 @@ let render_projects = (slug) => {
             categories: ['webdev']
         },
         {
-            image: 'assets/images/soot-spirits.png',
-            link: 'https://github.com/abhn/Soot-Spirits',
-            title: 'Soot Spirits',
-            demo: 'https://sootspirits.github.io',
-            technologies: ['Jekyll', 'CSS3'],
-            description: "A simple responsive two column Jekyll theme. Great for personal blog and basic portfolio website.",
-            categories: ['webdev']
-        },
-        {
-            image: 'assets/images/python-chat.png',
-            link: 'https://www.nagekar.com/2014/12/lan-group-messenger-in-python.html',
-            title: 'Terminal Group Chat',
-            demo: false,
-            technologies: ['Python', 'Sockets'],
-            description: "Simple terminal group chat based on native sockets using Python.",
-            categories: ['native']
-        },
-        {
             image: 'assets/images/old-lcd.jpg',
             link: 'https://www.nagekar.com/2018/05/reusing-old-laptop-lcd-panel.html',
             title: 'Reusing Old LCD Panel',
@@ -117,42 +90,6 @@ let render_projects = (slug) => {
             technologies: ['DIY'],
             description: "Reusing a dead laptop's LCD panel as a secondary monitor.",
             categories: ['diy']
-        },
-        {
-            image: 'assets/images/nextcloud-enc.png',
-            link: 'https://www.nagekar.com/2017/08/private-cloud-part-2.html',
-            title: 'Encrypted Self-Hosted Cloud',
-            demo: false,
-            technologies: ['NextCloud', 'GnuPG'],
-            description: "Self hosted encrypted cloud setup with Nextcloud and GnuPG.",
-            categories: ['diy', 'security']
-        },
-        {
-            image: 'assets/images/google-cloud-backup.png',
-            link: 'https://www.nagekar.com/2018/05/encrypted-backup-with-duplicity.html',
-            title: 'Encrypted Backups - Google Cloud',
-            demo: false,
-            technologies: ['NextCloud', 'Duplicity'],
-            description: "Create automated encrypted incremental backups of data. Sync everything securely to Google Cloud.",
-            categories: ['diy', 'security']
-        },
-        {
-            image: 'assets/images/pi-cloud.jpg',
-            link: 'https://www.nagekar.com/2016/01/how-to-private-local-cloud-using-raspberrypi.html',
-            title: 'Local Cloud - Raspberry Pi',
-            demo: false,
-            technologies: ['FTP', 'DIY'],
-            description: "Host a local cloud server with a Raspberry Pi and a spare hard disk. Access data instantaneously on any device on the network.",
-            categories: ['diy']
-        },
-        {
-            image: 'assets/images/koalamate.png',
-            link: 'https://github.com/abhn/koalamate',
-            title: 'Koalamate',
-            demo: false,
-            technologies: ['Electron', 'Javascript'],
-            description: "A cross-platform desktop application that serves as a Wolfram Alpha query place and notes taker.",
-            categories: ['native']
         },
     ]
 
